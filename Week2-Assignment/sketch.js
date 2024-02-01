3e7543bc7983664e85b69b8871add585c6130dd6
Guanyan Lu <54307226+frankylff@users.noreply.github.com>
2024-02-01 16:41:00 -0500

    homework

--- Week2-Assignment/sketch.js
+++ Week2-Assignment/sketch.js
@@ -4,26 +4,26 @@ function setup() {
   }
   
   function draw() {
-    let drawingColor = color(255, 0, 0); // 初始绘图颜色为红色
-    let brushSize = 10; // 初始笔刷大小
+    let drawingColor = color(255, 0, 0);
+    let brushSize = 10;
   
-    if (mouseIsPressed) { // 当鼠标被按下时
-      noStroke(); // 不要笔划
-      fill(drawingColor); // 填充颜色
-      ellipse(mouseX, mouseY, brushSize, brushSize); // 在鼠标位置画圆形
+    if (mouseIsPressed) {
+      noStroke();
+      fill(drawingColor);
+      ellipse(mouseX, mouseY, brushSize, brushSize);
     }
   }
   
   function keyPressed() {
-    if (key === 'r') { // 当按下 'r' 键时，将绘图颜色设置为红色
+    if (key === 'r') {
       drawingColor = color(255, 0, 0);
-    } else if (key === 'g') { // 当按下 'g' 键时，将绘图颜色设置为绿色
+    } else if (key === 'g') {
       drawingColor = color(0, 255, 0);
-    } else if (key === 'b') { // 当按下 'b' 键时，将绘图颜色设置为蓝色
+    } else if (key === 'b') {
       drawingColor = color(0, 0, 255);
-    } else if (key === '+') { // 当按下 '+' 键时，增加笔刷大小
+    } else if (key === '+') {
       brushSize += 5;
-    } else if (key === '-') { // 当按下 '-' 键时，减少笔刷大小
-      brushSize = max(5, brushSize - 5); // 确保笔刷大小不小于5
+    } else if (key === '-') {
+      brushSize = max(5, brushSize - 5);
     }
   }
