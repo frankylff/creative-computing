2f2f1ff23bf443d6595df6182ca90e6e4bbb34a8
frankylff <1164353811@qq.com>
2024-02-01 20:51:33 -0500

    W3

--- Inclass/Code-Week3/socket_server/client/sketch.js
+++ Inclass/Code-Week3/socket_server/client/sketch.js
@@ -1,29 +1,16 @@
+let ws;
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(220);
-  }
-  
-  function draw() {
-    let drawingColor = color(255, 0, 0);
-    let brushSize = 10;
-  
-    if (mouseIsPressed) {
-      noStroke();
-      fill(drawingColor);
-      ellipse(mouseX, mouseY, brushSize, brushSize);
-    }
-  }
-  
-  function keyPressed() {
-    if (key === 'r') {
-      drawingColor = color(255, 0, 0);
-    } else if (key === 'g') {
-      drawingColor = color(0, 255, 0);
-    } else if (key === 'b') {
-      drawingColor = color(0, 0, 255);
-    } else if (key === '+') {
-      brushSize += 5;
-    } else if (key === '-') {
-      brushSize = max(5, brushSize - 5);
-    }
-  }
+  const canvas = createCanvas(windowWidth, windowHeight);
+  canvas.addClass('background');
+  background(220);
+
+  ws = new WebSocket("ws://localhost:3000")
+}
+
+function mouseDragged() {
+  line(pmouseX, pmouseY, mouseX, mouseY);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
