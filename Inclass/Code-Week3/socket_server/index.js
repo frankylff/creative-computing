a53a071fe0bd9a5a64bd10aa487cf998b43b272d
frankylff <1164353811@qq.com>
2024-02-01 20:09:28 -0500

    W3

---
+++ Inclass/Code-Week3/socket_server/index.js
@@ -0,0 +1,15 @@
+import {WebSockectServer} from 'ws';
+const wss = new WebSocketServer({port: 3000});
+wss.on('connection', onConnection);
+
+function onConnection (ws){
+    ws.on('message', onMessage);
+}
+
+function onMessage(data){
+    let jsonData = JSON.parse(data);
+    for (let i = 0; i < wss.clients.length; i++){
+        const c = wss.clients[i];
+        c.send(data);
+    }
+}
