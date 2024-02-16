17d7efe8c04650e513176cbe316015c05f89dacc
frankylff <1164353811@qq.com>
2024-02-15 19:53:51 -0500

    W5

--- Inclass/Code-Week5/CitiBike/sketch.js
+++ Inclass/Code-Week5/CitiBike/sketch.js
@@ -1,5 +1,11 @@
+let data;
+function preload() {
+    data = loadJSON('./Data/2023-11-06.json')
+}
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
+    console.log('station', data.stations.lenght);
 }
 
 function draw() {
