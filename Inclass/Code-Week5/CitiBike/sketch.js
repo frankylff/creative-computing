17ad25402e171f1d2dd26f6928e810f2bdd7fd57
frankylff <1164353811@qq.com>
2024-02-15 20:27:06 -0500

    W5

--- Inclass/Code-Week5/CitiBike/sketch.js
+++ Inclass/Code-Week5/CitiBike/sketch.js
@@ -1,11 +1,15 @@
 let data;
+let maxLat, maxLon, minLat, minLon;
+
 function preload() {
-    data = loadJSON('./Data/2023-11-06.json')
+    data = loadJSON('./data/2023-11-06.json')
 }
 
 function setup() {
     createCanvas(600, 600);
-    console.log('station', data.stations.lenght);
+    console.log('stations:', data.stations.lenght);
+    const lats = data.stations.map(station => station.lat);
+    maxLat = Math.max(...lats);
 }
 
 function draw() {
