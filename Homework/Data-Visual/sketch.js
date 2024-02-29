e78fccbfa8d4c5431a6d5bae83d143f0569d54a5
Guanyan Lu <1164353811@qq.com>
2024-02-29 16:01:17 -0500

    HW

--- Homework/Data-Visual/sketch.js
+++ Homework/Data-Visual/sketch.js
@@ -1,3 +1,4 @@
+let bg;
 let data;
 let tempData = [];
 let rainData = [];
@@ -7,15 +8,24 @@ let maxYearR = -Infinity;
 let minYearR = Infinity;
 let tempColor = [];
 
+let currentIndex = 0;
+let drawTemp = false;
+let drawRain = false;
+let startTime;
+
 function preload(){
     data = loadTable("Data/ZhengzhouWeather.csv", "header");
+    bg = loadImage('Data/Background.jpg');
 }
 
 function setup() {
     createCanvas(800, 800)
+    background(bg);
     getTemp();
     getRain()
     getTempColor();
+
+    startTime = millis();
 }
 
 function getRain(){
@@ -119,53 +129,58 @@ function getTempColor(){
 }
 
 function displayT(){
-    for(let i = 0; i < tempData.length; i++){
-        let temp = tempData[i];
+    let temp = tempData[currentIndex];
+
+    push();
+    translate(width/2, height/2);
+
+    let angle = map(currentIndex, 0, tempData.length, 0, TWO_PI);
+    rotate(angle);
+
+    let colorIndex = int(map(temp["meanT"], -8, 41, tempColor.length, 0));
+    let color = tempColor[colorIndex];
+    stroke(color, 60, 90);
+    strokeWeight(3);
+    noFill();
+    
+    let lowY = map(temp["minT"], -8, 41, 50, 350);
+    let highY = map(temp["maxT"], -8, 41, 50, 350);
+    line(0, lowY, 0, highY);
+
+    pop();
+}
+
+function displayR(){
+    let rain = rainData[currentIndex];
+    let r = map(Number(rain["meanR"]), 0, 65, 0, 50);
+
+    if(r > 2){
+        let temp = tempData[currentIndex];
+        let angle = map(currentIndex, 0, tempData.length, 0, TWO_PI);
+        let locY = map(temp["meanT"], -8, 41, 100, 300)
 
         push();
         translate(width/2, height/2);
-
-        let angle = map(i, 0, tempData.length, 0, TWO_PI);
         rotate(angle);
-
-        let colorIndex = int(map(temp["meanT"], -8, 41, tempColor.length, 0));
-        let color = tempColor[colorIndex];
-        stroke(color, 60, 90);
-        strokeWeight(3);
-        noFill();
-        
-        let lowY = map(temp["minT"], -8, 41, 50, 350);
-        let highY = map(temp["maxT"], -8, 41, 50, 350);
-        line(0, lowY, 0, highY);
-
+        stroke(360, 0, 100, 100);
+        fill(200, 80, 100, 50);
+        circle(0, locY, r*2);
         pop();
     }
 }
 
-function displayR(){
-    for(let i = 0; i < rainData.length; i++){
-        let rain = rainData[i];
-        let r = map(Number(rain["meanR"]), 0, 65, 0, 50);
-
-        if(r>2){
-            let temp = tempData[i];
-            let angle = map(i, 0, tempData.length, 0, TWO_PI);
-            let locY = map(temp["meanT"], -8, 41, 100, 300)
-
-            push();
-            translate(width/2, height/2);
-            rotate(angle);
-            stroke(360);
-            strokeWeight(3);
-            fill(220, 80, 80, 30);
-            circle(0, locY, r*2);
-            pop();
+function draw() {
+    if (millis() - startTime >= 100 * currentIndex && currentIndex < tempData.length) {
+        if (!drawTemp) {
+            displayT();
+            drawTemp = true;
+        } else if (!drawRain) {
+            displayR();
+            drawRain = true;
+        } else {
+            currentIndex++;
+            drawTemp = false;
+            drawRain = false;
         }
     }
-}
-
-function draw() {
-    displayT();
-    displayR();
-    noLoop();
 }
